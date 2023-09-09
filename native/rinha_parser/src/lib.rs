f9b2060deb1dbbe8645de8b6a159408ff9c834ea
Maxsuel Fernandes Maccari <maxmaccari@hotmail.com>
2023-09-09 15:37:32 -0400

    Implement rustler rinha file parsing

--- native/rinha_parser/src/lib.rs
+++ native/rinha_parser/src/lib.rs
@@ -1,6 +1,13 @@
+use rinha::parser::{parse_or_report, ParseError};
+use std::fs::read_to_string;
+
 #[rustler::nif]
-fn parse_rinha(content: &str) -> String {
-    format!("Testing {content}...").to_owned()
+fn parse_rinha(filename: &str) -> Result<String, String> {
+    let content = read_to_string(filename).map_err(|e| e.to_string())?;
+    let file = parse_or_report(filename, &content).map_err(|e: ParseError| e.to_string())?;
+    let json = serde_json::to_string(&file).map_err(|e| e.to_string())?;
+
+    Ok(json)
 }
 
 rustler::init!("Elixir.RinhaCompiler.RinhaParser", [parse_rinha]);
