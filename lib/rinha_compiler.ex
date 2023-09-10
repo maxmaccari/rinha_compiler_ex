e7340b2356dacbb2d4082f10d52985cb36abb36d
Maxsuel Fernandes Maccari <maxmaccari@hotmail.com>
2023-09-10 08:54:27 -0400

    Add base functions

--- lib/rinha_compiler.ex
+++ lib/rinha_compiler.ex
@@ -1,10 +1,59 @@
 defmodule RinhaCompiler do
   alias RinhaCompiler.{RinhaParser, ElixirAstParser}
 
+  @type elixir_ast :: tuple()
+
+  @doc """
+  Compile rinha file to elixir ast.
+
+  It returns a tuple with the elixir ast, and the module name.
+  """
+  @spec compile(String.t()) :: {:ok, elixir_ast(), atom()} | {:error, String.t()}
   def compile(filename) do
-    with {:ok, rinha_ast} <- RinhaParser.parse(filename) do
-      rinha_ast
-      |> ElixirAstParser.parse()
+    with {:ok, rinha_ast, module_name} <- RinhaParser.parse(filename) do
+      {:ok, ElixirAstParser.parse(rinha_ast), module_name}
+    end
+  end
+
+  @doc """
+  Compile rinha file to elixir ast.
+
+  It trows in errors.
+  """
+  def compile!(filename) do
+    case compile(filename) do
+      {:ok, elixir_ast, _module_name} -> elixir_ast
+      {:error, error} -> raise "Error: #{error}"
+    end
+  end
+
+  @doc """
+  Compile rinha file, and run its code.
+  """
+  def eval(filename) do
+    with {:ok, elixir_ast, module_name} <- compile(filename) do
+      Code.eval_quoted(elixir_ast)
+      apply(module_name, :run, [])
+    end
+  end
+
+  @doc """
+  Compile rinha file to Elixir code.
+  """
+  @spec eval(binary) :: {:ok, String.t()} | {:error, String.t()}
+  def to_elixir(filename) do
+    with {:ok, elixir_ast, _module_name} <- compile(filename) do
+      {:ok, Macro.to_string(elixir_ast)}
+    end
+  end
+
+  @doc """
+  Compile rinha file to Elixir code.
+  """
+  def to_elixir!(filename) do
+    case to_elixir(filename) do
+      {:ok, elixir_code} -> elixir_code
+      {:error, error} -> raise "Error: #{error}"
     end
   end
 end
--- lib/rinha_compiler/rinha_parser.ex
+++ lib/rinha_compiler/rinha_parser.ex
@@ -8,11 +8,14 @@ defmodule RinhaCompiler.RinhaParser do
   @doc false
   def parse_rinha(_filename), do: :erlang.nif_error(:nif_not_loaded)
 
-  @spec parse(String.t()) :: {:ok, File.t()} | {:error, String.t()}
+  @spec parse(String.t()) :: {:ok, File.t(), atom()}
   def parse(filename) do
     with {:ok, ast_json} <- parse_rinha(filename),
          {:ok, ast_json_decoded} <- Jason.decode(ast_json) do
-      {:ok, File.new(ast_json_decoded)}
+      file = File.new(ast_json_decoded)
+      module_name = File.module_name(file)
+
+      {:ok, file, module_name}
     end
   end
 end
